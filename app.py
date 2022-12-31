f40008c23fe990000e9099a70cef0044709bca78
ashhadahsan <ashhadahsan@gmail.com>
2022-12-31 22:23:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,19 +17,12 @@ cors = CORS(
     # },
 )
 import sqlite3 as sql
-from flask_restx import Api, Resource
-
-api = Api(app)
 
 conn = sql.connect("database.db")
-
 
 conn.execute(
     "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT,username VARCHAR(50) NOT NULL,password VARCHAR(50) NOT NULL,email VARCHAR(50) NOT NULL)"
 )
-
-
-app = Flask(__name__)
 
 
 @app.route("/")
@@ -55,278 +48,276 @@ con.close()
 import random
 
 
-@api.route("/api/books/featured")
-class Random(Resource):
-    def get(self):
-        data = request.get_json()
-        n = data["n"]
-        try:
-            idx = random.sample(range(0, books.shape[0]), k=n)
+@app.route("/api/books/featured")
+def get_random(n=10):
+    data = request.get_json()
+    n = data["n"]
+    try:
+        idx = random.sample(range(0, books.shape[0]), k=n)
 
-            featured = books.loc[
-                idx,
-                [
-                    "book_id",
-                    "title",
-                    "original_title",
-                    "authors",
-                    "original_publication_year",
-                    "average_rating",
-                    "large_image_url",
-                    "image_url",
-                ],
-            ].to_dict(orient="records")
-            # response = app.response_class(
-            #     response=json.dumps(featured), status=200, mimetype="application/json"
-            # )
-            return {featured}, 200
-        except:
-            return {"Internal Server Error"}, 500
-            # response = app.response_class(
-            #     response=json.dumps("Interal Server Error"),
-            #     status=500,
-            #     mimetype="application/json",
-            # )
+        featured = books.loc[
+            idx,
+            [
+                "book_id",
+                "title",
+                "original_title",
+                "authors",
+                "original_publication_year",
+                "average_rating",
+                "large_image_url",
+                "image_url",
+            ],
+        ].to_dict(orient="records")
+        response = app.response_class(
+            response=json.dumps(featured), status=200, mimetype="application/json"
+        )
+        return response
+    except:
+        response = app.response_class(
+            response=json.dumps("Interal Server Error"),
+            status=500,
+            mimetype="application/json",
+        )
 
 
-# @app.route("/api/books/search", methods=["GET", "POST"])
-# def find():
+@app.route("/api/books/search", methods=["GET", "POST"])
+def find():
 
-#     con = sql.connect("database.db")
+    con = sql.connect("database.db")
 
-#     data = request.get_json()
-#     param = data["query"]
-#     n = data["n"]
+    data = request.get_json()
+    param = data["query"]
+    n = data["n"]
 
-#     search = pd.read_sql(
-#         f'''SELECT * from books where original_title like "%{param}%"''', con
-#     )
-#     search["large_image_url"] = search["small_image_url"].apply(
-#         lambda x: get_large_url(x)
-#     )
-#     search["original_title"] = search["original_title"].str.title()
-#     search = search.reset_index()
-#     results = (
-#         search.head(n)
-#         .loc[
-#             :,
-#             [
-#                 "book_id",
-#                 "title",
-#                 "original_title",
-#                 "authors",
-#                 "original_publication_year",
-#                 "average_rating",
-#                 "large_image_url",
-#                 "image_url",
-#             ],
-#         ]
-#         .to_dict(orient="records")
-#     )
-#     if len(results) > 0:
-#         response = app.response_class(
-#             response=json.dumps(results), status=200, mimetype="application/json"
-#         )
-#         con.close()
-#         return response
-#     else:
-#         response = app.response_class(
-#             response=json.dumps("Not found"), status=404, mimetype="application/json"
-#         )
-#         con.close()
-#         return response
-
-
-# with open("recommendation_results.json", "r") as j:
-#     contents = json.loads(j.read())
-
-# import functools
-# import operator
+    search = pd.read_sql(
+        f'''SELECT * from books where original_title like "%{param}%"''', con
+    )
+    search["large_image_url"] = search["small_image_url"].apply(
+        lambda x: get_large_url(x)
+    )
+    search["original_title"] = search["original_title"].str.title()
+    search = search.reset_index()
+    results = (
+        search.head(n)
+        .loc[
+            :,
+            [
+                "book_id",
+                "title",
+                "original_title",
+                "authors",
+                "original_publication_year",
+                "average_rating",
+                "large_image_url",
+                "image_url",
+            ],
+        ]
+        .to_dict(orient="records")
+    )
+    if len(results) > 0:
+        response = app.response_class(
+            response=json.dumps(results), status=200, mimetype="application/json"
+        )
+        con.close()
+        return response
+    else:
+        response = app.response_class(
+            response=json.dumps("Not found"), status=404, mimetype="application/json"
+        )
+        con.close()
+        return response
 
 
-# @app.route("/api/books/recommend", methods=["GET", "POST"])
-# def recommend():
-#     data = request.get_json()
-#     id = data["id"]
-#     try:
-#         r = contents.get(str(books[books["book_id"] == id].index.tolist()[0]))[:5]
-#         index = functools.reduce(operator.iconcat, r, [])[1::2]
-#         index = [int(x) for x in index]
-#         recommendations = books.loc[
-#             index,
-#             [
-#                 "book_id",
-#                 "title",
-#                 "original_title",
-#                 "authors",
-#                 "original_publication_year",
-#                 "average_rating",
-#                 "large_image_url",
-#                 "image_url",
-#             ],
-#         ].to_dict(orient="records")
-#         response = app.response_class(
-#             response=json.dumps(recommendations),
-#             status=200,
-#             mimetype="application/json",
-#         )
-#         return response
-#     except:
-#         response = app.response_class(
-#             response=json.dumps("Not Found"), status=404, mimetype="application/json"
-#         )
-#         return response
+with open("recommendation_results.json", "r") as j:
+    contents = json.loads(j.read())
+
+import functools
+import operator
 
 
-# @app.route("/api//books/details", methods=["GET", "POST"])
-# def get_by_id():
-#     data = request.get_json()
-#     book_id = data["id"]
-
-#     book = books[books["goodreads_book_id"] == book_id][
-#         [
-#             "book_id",
-#             "title",
-#             "original_title",
-#             "authors",
-#             "original_publication_year",
-#             "average_rating",
-#             "large_image_url",
-#             "image_url",
-#         ]
-#     ].to_dict(orient="records")
-#     if len(book) > 0:
-
-#         response = app.response_class(
-#             response=json.dumps(book), status=200, mimetype="application/json"
-#         )
-#         return response
-#     else:
-#         response = app.response_class(
-#             response=json.dumps("Not Found"), status=404, mimetype="application/json"
-#         )
-#         return response
-
-
-# @app.route("/api/books/top", methods=["GET", "POST"])
-# def top_rated():
-#     data = request.get_json()
-#     n = data["n"]
-#     try:
-#         top_books = (
-#             books.sort_values(by="average_rating", ascending=False)
-#             .head(50)
-#             .reset_index()
-#         )
-#         idx = random.sample(range(0, top_books.shape[0]), k=n)
-#         top = top_books.loc[
-#             idx,
-#             [
-#                 "book_id",
-#                 "title",
-#                 "original_title",
-#                 "authors",
-#                 "original_publication_year",
-#                 "average_rating",
-#                 "large_image_url",
-#                 "image_url",
-#             ],
-#         ].to_dict(orient="records")
-
-#         response = app.response_class(
-#             response=json.dumps(top), status=200, mimetype="application/json"
-#         )
-#         return response
-#     except:
-#         response = app.response_class(
-#             response=json.dumps("Internal Server Error"),
-#             status=500,
-#             mimetype="application/json",
-#         )
-#         return response
+@app.route("/api/books/recommend", methods=["GET", "POST"])
+def recommend():
+    data = request.get_json()
+    id = data["id"]
+    try:
+        r = contents.get(str(books[books["book_id"] == id].index.tolist()[0]))[:5]
+        index = functools.reduce(operator.iconcat, r, [])[1::2]
+        index = [int(x) for x in index]
+        recommendations = books.loc[
+            index,
+            [
+                "book_id",
+                "title",
+                "original_title",
+                "authors",
+                "original_publication_year",
+                "average_rating",
+                "large_image_url",
+                "image_url",
+            ],
+        ].to_dict(orient="records")
+        response = app.response_class(
+            response=json.dumps(recommendations),
+            status=200,
+            mimetype="application/json",
+        )
+        return response
+    except:
+        response = app.response_class(
+            response=json.dumps("Not Found"), status=404, mimetype="application/json"
+        )
+        return response
 
 
-# @app.route("/api/user/login", methods=["GET", "POST"])
-# def login():
-#     msg = ""
-#     data = request.get_json()
+@app.route("/api//books/details", methods=["GET", "POST"])
+def get_by_id():
+    data = request.get_json()
+    book_id = data["id"]
 
-#     username = data["username"]
-#     password = data["password"]
-#     with sql.connect("database.db") as con:
-#         cur = con.cursor()
-#         cur.execute(
-#             "SELECT * FROM users WHERE username =? AND password = ?",
-#             (
-#                 username,
-#                 password,
-#             ),
-#         )
-#         account = cur.fetchone()
-#         # print(account)
-#     if account:
-#         # session['loggedin'] = True
-#         # session['id'] = account['id']
-#         # session['username'] = account['username']
-#         msg = "Logged in successfully !"
-#         response = app.response_class(
-#             response=json.dumps(msg), status=200, mimetype="application/json"
-#         )
-#         return response
-#     else:
-#         msg = "Incorrect username / password !"
-#         response = app.response_class(
-#             response=json.dumps(msg), status=401, mimetype="application/json"
-#         )
-#         return response
+    book = books[books["goodreads_book_id"] == book_id][
+        [
+            "book_id",
+            "title",
+            "original_title",
+            "authors",
+            "original_publication_year",
+            "average_rating",
+            "large_image_url",
+            "image_url",
+        ]
+    ].to_dict(orient="records")
+    if len(book) > 0:
+
+        response = app.response_class(
+            response=json.dumps(book), status=200, mimetype="application/json"
+        )
+        return response
+    else:
+        response = app.response_class(
+            response=json.dumps("Not Found"), status=404, mimetype="application/json"
+        )
+        return response
 
 
-# @app.route("/api/user/logout", methods=["GET", "POST"])
-# def logout():
-#     session.pop("loggedin", None)
-#     session.pop("id", None)
-#     session.pop("username", None)
-#     return redirect(url_for("login"))
+@app.route("/api/books/top", methods=["GET", "POST"])
+def top_rated():
+    data = request.get_json()
+    n = data["n"]
+    try:
+        top_books = (
+            books.sort_values(by="average_rating", ascending=False)
+            .head(50)
+            .reset_index()
+        )
+        idx = random.sample(range(0, top_books.shape[0]), k=n)
+        top = top_books.loc[
+            idx,
+            [
+                "book_id",
+                "title",
+                "original_title",
+                "authors",
+                "original_publication_year",
+                "average_rating",
+                "large_image_url",
+                "image_url",
+            ],
+        ].to_dict(orient="records")
+
+        response = app.response_class(
+            response=json.dumps(top), status=200, mimetype="application/json"
+        )
+        return response
+    except:
+        response = app.response_class(
+            response=json.dumps("Internal Server Error"),
+            status=500,
+            mimetype="application/json",
+        )
+        return response
 
 
-# @app.route("/api/user/register", methods=["GET", "POST"])
-# def register():
-#     msg = ""
-#     data = request.get_json()
+@app.route("/api/user/login", methods=["GET", "POST"])
+def login():
+    msg = ""
+    data = request.get_json()
 
-#     username = data["username"]
-#     password = data["password"]
-#     email = data["email"]
-#     with sql.connect("database.db") as con:
+    username = data["username"]
+    password = data["password"]
+    with sql.connect("database.db") as con:
+        cur = con.cursor()
+        cur.execute(
+            "SELECT * FROM users WHERE username =? AND password = ?",
+            (
+                username,
+                password,
+            ),
+        )
+        account = cur.fetchone()
+        # print(account)
+    if account:
+        # session['loggedin'] = True
+        # session['id'] = account['id']
+        # session['username'] = account['username']
+        msg = "Logged in successfully !"
+        response = app.response_class(
+            response=json.dumps(msg), status=200, mimetype="application/json"
+        )
+        return response
+    else:
+        msg = "Incorrect username / password !"
+        response = app.response_class(
+            response=json.dumps(msg), status=401, mimetype="application/json"
+        )
+        return response
 
-#         cur = con.cursor()
 
-#         cur.execute("SELECT * FROM users WHERE username = ?", (username,))
-#         account = cur.fetchone()
-#         # print(account)
+@app.route("/api/user/logout", methods=["GET", "POST"])
+def logout():
+    session.pop("loggedin", None)
+    session.pop("id", None)
+    session.pop("username", None)
+    return redirect(url_for("login"))
 
-#         if account:
-#             msg = "Account already exists !"
-#             response = app.response_class(
-#                 response=json.dumps(msg), status=403, mimetype="application/json"
-#             )
-#             return response
 
-#         else:
-#             with sql.connect("database.db") as con:
-#                 cur = con.cursor()
-#                 cur.execute(
-#                     "INSERT INTO users (username,password,email) VALUES (?,?,?)",
-#                     (username, password, email),
-#                 )
+@app.route("/api/user/register", methods=["GET", "POST"])
+def register():
+    msg = ""
+    data = request.get_json()
 
-#                 con.commit()
-#             msg = "You have successfully registered !"
-#             response = app.response_class(
-#                 response=json.dumps(msg), status=200, mimetype="application/json"
-#             )
-#             return response
+    username = data["username"]
+    password = data["password"]
+    email = data["email"]
+    with sql.connect("database.db") as con:
+
+        cur = con.cursor()
+
+        cur.execute("SELECT * FROM users WHERE username = ?", (username,))
+        account = cur.fetchone()
+        # print(account)
+
+        if account:
+            msg = "Account already exists !"
+            response = app.response_class(
+                response=json.dumps(msg), status=403, mimetype="application/json"
+            )
+            return response
+
+        else:
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute(
+                    "INSERT INTO users (username,password,email) VALUES (?,?,?)",
+                    (username, password, email),
+                )
+
+                con.commit()
+            msg = "You have successfully registered !"
+            response = app.response_class(
+                response=json.dumps(msg), status=200, mimetype="application/json"
+            )
+            return response
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", debug=False)
