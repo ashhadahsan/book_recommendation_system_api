8d025799bfc8eec5a3577b00874d7765b59fd9b3
ashhadahsan <ashhadahsan@gmail.com>
2022-12-31 17:14:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config["CORS_HEADERS"] = "Content-Type"
 cors = CORS(
     app,
     resources={
-        r"/api/*": {
+        r"/*": {
             "origins": [
                 "http://localhost:3000",
             ]
