35c355c54792c803c8a42c18653f28195b276d9e
ashhadahsan <63206988+ashhadahsan@users.noreply.github.com>
2023-03-22 18:57:29 +0500

    update

--- app.py
+++ app.py
@@ -28,6 +28,11 @@ con.execute(
     FOREIGN KEY (user_id)
     REFERENCES users(id))"""
 )
+con.execute(
+    """CREATE TABLE IF NOT EXISTS search_history (history_id INTEGER PRIMARY KEY AUTOINCREMENT,book_id INTEGER,user_id INTEGER NOT NULL, CONSTRAINT fk_users
+    FOREIGN KEY (user_id)
+    REFERENCES users(id))"""
+)
 
 
 @app.route("/")
@@ -43,13 +48,68 @@ def add_fvrt():
     con = sql.connect("database.db")
     cursor = con.cursor()
     cursor.execute(
-        f"""INSERT into fvrts (book_id,user_id) VALUES (?,?)""", (book_id, user_id)
+        f"""INSERT into fvrts (book_id,user_id) VALUES (?,?)""",
+        (book_id, user_id),
     )
     con.commit()
     con.close()
 
     response = app.response_class(
         response=json.dumps({"status": True}), status=200, mimetype="application/json"
+    )
+    return response
+
+
+@app.route("/api/books/addHistory", methods=["POST"])
+def add_history():
+    request_d = request.get_json()
+    user_id = request_d["user_id"]
+    book_id = request_d["book_id"]
+    con = sql.connect("database.db")
+    cursor = con.cursor()
+    cursor.execute(
+        f"""INSERT into search_history  (book_id,user_id) VALUES (?,?)""",
+        (book_id, user_id),
+    )
+    con.commit()
+    con.close()
+
+    response = app.response_class(
+        response=json.dumps({"status": True}), status=200, mimetype="application/json"
+    )
+    return response
+
+
+@app.route("/api/books/getHistory", methods=["POST"])
+def getHistory():
+    data = request.get_json()
+    user_id = data["user_id"]
+    conn = sql.connect("database.db")
+    book_ids = pd.read_sql(
+        f"""SELECT book_id from search_history WHERE user_id={user_id} """, conn
+    )
+    print(book_ids)
+
+    book = book_ids["book_id"]
+    book = book.tolist()
+    id = [str(x) for x in book]
+
+    books_selected = books[books["book_id"].isin(id)][
+        [
+            "book_id",
+            "title",
+            "original_title",
+            "authors",
+            "original_publication_year",
+            "average_rating",
+            "large_image_url",
+            "image_url",
+        ]
+    ]
+    response = app.response_class(
+        response=json.dumps(books_selected.to_dict(orient="records")),
+        status=200,
+        mimetype="application/json",
     )
     return response
 
@@ -62,20 +122,25 @@ def rem_fvrt():
         book_id = request_d["book_id"]
         con = sql.connect("database.db")
         cursor = con.cursor()
-        cursor.execute("""DELETE FROM fvrts WHERE book_id =? AND user_id =?""",(book_id,user_id))
+        cursor.execute(
+            """DELETE FROM fvrts WHERE book_id =? AND user_id =?""", (book_id, user_id)
+        )
         con.commit()
         con.close()
 
         response = app.response_class(
-            response=json.dumps({"status": True}), status=200, mimetype="application/json"
+            response=json.dumps({"status": True}),
+            status=200,
+            mimetype="application/json",
         )
         return response
     except:
         response = app.response_class(
-            response=json.dumps({"status": False}), status=400, mimetype="application/json"
+            response=json.dumps({"status": False}),
+            status=400,
+            mimetype="application/json",
         )
         return response
-
 
 
 @app.route("/api/books/getFvrts", methods=["POST"])
@@ -213,12 +278,14 @@ with open("recommendation_results.json", "r") as j:
 
 import functools
 import operator
+import random
 
 
 @app.route("/api/books/recommend", methods=["POST"])
 def recommend():
     data = request.get_json()
     id = data["id"]
+
     try:
         r = contents.get(str(books[books["book_id"] == id].index.tolist()[0]))[:5]
         index = functools.reduce(operator.iconcat, r, [])[1::2]
@@ -247,6 +314,49 @@ def recommend():
             response=json.dumps("Not Found"), status=404, mimetype="application/json"
         )
         return response
+
+
+@app.route("/api/books/recommendFromFvrts", methods=["POST"])
+def recommend_from_fvrts():
+    data = request.get_json()
+    user_id = data["user_id"]
+    conn = sql.connect("database.db")
+    book_ids = pd.read_sql(
+        f"""SELECT book_id from fvrts WHERE user_id={user_id} """, conn
+    )
+
+    book = book_ids["book_id"]
+    book = book.tolist()
+    id = [str(x) for x in book]
+
+    try:
+        id = id[0]
+        r = contents.get(str(books[books["book_id"] == id].index.tolist()[0]))[:5]
+        index = functools.reduce(operator.iconcat, r, [])[1::2]
+        index = [int(x) for x in index]
+        recommendations = books.loc[
+            index,
+            [
+                "book_id",
+                "title",
+                "original_title",
+                "authors",
+                "original_publication_year",
+                "average_rating",
+                "large_image_url",
+                "image_url",
+            ],
+        ].to_dict(orient="records")
+        response = app.response_class(
+            response=json.dumps(recommendations),
+            status=200,
+            mimetype="application/json",
+        )
+        return response
+    except:
+        response = app.response_class(
+            response=json.dumps("Not Found"), status=404, mimetype="application/json"
+        )
 
 
 @app.route("/api/books/details", methods=["POST"])
